b621897f420f548b2248f1107f6b5c780c7d49ca
Joel Kouakou <kouakoujoel7@gmail.com>
2024-12-10 15:21:59 +0000

    basic operations finished

---
+++ basic_operations.py
@@ -0,0 +1,15 @@
+# Creating two variables number1 and number 2
+number1 = 10
+number2 = 5
+
+# Addition operation
+result1 = number1 + number2
+print("Addition of",number1,"and",number2,"is",result1)
+
+# Subtraction operation
+result2 = number1 - number2
+print("Subtraction of",number1,"and",number2,"is",result2)
+
+# Multiplication 
+result3 = number1 * number2
+print("Multiplication of",number1,"and",number2,"is",result3)
