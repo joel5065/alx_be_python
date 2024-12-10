88415aa460e0d49edefce6a9de87e94a02b1ee24
Joel Kouakou <kouakoujoel7@gmail.com>
2024-12-10 16:29:33 +0000

    finished future age calculation

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+# Python and perform a simple arithmetic operation to calculate the user’s age in a future year.
+user_age = int(input("How are you? "))
+
+# Calculating future age for the user 
+future_age =  user_age + 27
+
+print("In 2050, you will be", future_age ,"years old.")
