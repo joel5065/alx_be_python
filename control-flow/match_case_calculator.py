56c02283611e9a60934be81bf41b13eb7b90f2b5
Joel Kouakou <kouakoujoel7@gmail.com>
2024-12-17 14:34:11 +0000

    Modified match case

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,9 +1,9 @@
 # Perform calculations using match case to select the operator
 
-number1 = int(input("Enter the first number"))
-number2 = int(input("Enter the second number:"))
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
 
-operator = str(input("Choose the operation (+, -, *, /):")).lower
+operator = str(input("Choose the operation (+, -, *, /): "))
 
 match operator:
     case operator if operator == "+":
@@ -13,6 +13,9 @@ match operator:
     case operator if operator == "*":
         print("The result is:", number1 * number2)
     case operator if operator == "/":
-        print("The result is:", number1 / number2)
+        if number2 == 0:
+            print("Cannot divide by zero")   
+        else:
+            print("The result is:", number1 / number2)
     case _:
         print("This operator is not allowed")
