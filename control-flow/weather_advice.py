3e0b33c501bc4379bfc53d1d30070dcb6dad14fb
Joel Kouakou <kouakoujoel7@gmail.com>
2024-12-17 14:01:38 +0000

    Modified weather_advice.py file

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,13 @@
+# This script gives you some advices regarding the weather
+# We will use if, elif and else
+
+user_response = str(input("What's the weather like today? (sunny/rainy/cold): ")).lower()
+
+if user_response == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif user_response == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif user_response == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
