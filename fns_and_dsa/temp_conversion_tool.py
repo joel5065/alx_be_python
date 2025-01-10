652a15e688dde94cb8b4e1465874916aab0871f4
Joel Kouakou <kouakoujoel7@gmail.com>
2025-01-10 16:16:57 +0000

    modified temp conversion tool file

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,10 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+
+def convert_to_celsius(fahrenheit):
+    return fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR
+    
+def convert_to_fahrenheit(celsius):
+    return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR
+
