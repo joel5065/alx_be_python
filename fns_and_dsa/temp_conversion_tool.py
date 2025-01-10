ed88e88b04a8cb72de8250e1f362961587ee3b74
Joel Kouakou <kouakoujoel7@gmail.com>
2025-01-10 16:57:37 +0000

    temp conversion tool file v003

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -8,16 +8,19 @@ def convert_to_celsius(fahrenheit):
 def convert_to_fahrenheit(celsius):
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
-temp_to_convert = input("Enter the temperature to convert: ")
-checker = str(input("Is this temperature in Celsius or Fahrenheit? (C/F)")).upper
-if temp_to_convert.isdigit():
-    if checker == "F":
-        output_temp = convert_to_celsius(temp_to_convert)
-        print(f"{temp_to_convert}°F is {output_temp}°C")
-    elif checker == "C":
-        output_temp = convert_to_fahrenheit(temp_to_convert)
-        print(f"{temp_to_convert}°C is {output_temp}°F")
-    else:
-        print("Enter a valid input")
+input_value = input("Enter the temperature to convert: ")
+if input_value.isdigit():
+    temp_to_convert = input_value
 else:
     print("Invalid temperature. Please enter a numeric value ")
+
+checker = str(input("Is this temperature in Celsius or Fahrenheit? (C/F)")).upper
+if checker == "F":
+    output_temp = convert_to_celsius(temp_to_convert)
+    print(f"{temp_to_convert}°F is {output_temp}°C")
+elif checker == "C":
+    output_temp = convert_to_fahrenheit(temp_to_convert)
+    print(f"{temp_to_convert}°C is {output_temp}°F")
+else:
+    print("Enter a valid input")
+    
