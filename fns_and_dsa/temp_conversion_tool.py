50a08d37661c8d3c0b2fe7ec0aa403ab6a693159
Joel Kouakou <kouakoujoel7@gmail.com>
2025-01-10 18:03:07 +0000

    adding value error

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -9,25 +9,20 @@ def convert_to_celsius(fahrenheit):
 def convert_to_fahrenheit(celsius):
     fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
     return fahrenheit
-while True:
-    try:
-        temp_to_convert = float(input("Enter the temperature to convert: "))
 
-        if temp_to_convert.is_integer:
+temp_to_convert = float(input("Enter the temperature to convert: "))
 
-            unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
-            if unit == "F":
-                output_temp = convert_to_celsius(temp_to_convert)
-                print(f"{temp_to_convert}°F is {output_temp}°C")
-                break
-            elif unit == "C":
-                output_temp = convert_to_fahrenheit(temp_to_convert)
-                print(f"{temp_to_convert:.1f}°C is {output_temp:.1f}°F")
-                break
-            else:
-                print("Enter a valid input")
-                break
-    except ValueError:
-            print("Invalid temperature. Please enter a numeric value ")
+if temp_to_convert.is_integer:
+    unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
+    if unit == "F":
+        output_temp = convert_to_celsius(temp_to_convert)
+        print(f"{temp_to_convert}°F is {output_temp}°C")
+    elif unit == "C":
+        output_temp = convert_to_fahrenheit(temp_to_convert)
+        print(f"{temp_to_convert:.1f}°C is {output_temp:.1f}°F")
+    else:
+        print("Enter a valid input")
+else:
+     raise ValueError("Invalid temperature. Please enter a numeric value ")
 
     
