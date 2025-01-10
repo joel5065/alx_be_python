8242fc04c759fcd96983fddbb01094f30c35126c
Joel Kouakou <kouakoujoel7@gmail.com>
2025-01-10 09:43:41 +0000

    insert elif inside the control loop

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,8 +7,10 @@ def perform_operation(num1, num2, operation):
         case "multiply":
             return num1 * num2
         case "divide":
-            if num2 == 0:
+            if num1 == 0:
                 return " Provide a number different of zero"
+            elif num2 == 0:
+                return "Provide a number different of zero"
             else:
                 return num1 / num2
 
