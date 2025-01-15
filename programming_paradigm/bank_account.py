a31a25e3c4c8bcee433fdf7d4699a01ea60b30ab
Joel Kouakou <kouakoujoel7@gmail.com>
2025-01-15 18:03:17 +0000

    modified bank_account file

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+   
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+    
+    def withdraw(self, amount):
+        if self.account_balance < amount:
+            return False
+        self.account_balance -= amount
+        return True
+    
+    def display_balance(self):
+        return f"The account balance is {self.account_balance}$"
+    
