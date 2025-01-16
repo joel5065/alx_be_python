6612f48b6a6ad2235f26e3e7d78e10d2671e0b80
Joel Kouakou <kouakoujoel7@gmail.com>
2025-01-16 08:10:24 +0000

    modifying the except block v001

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,5 +4,5 @@ def safe_divide(numerator : float, denominator: float):
         return f"The result of the division is {result:.1f}"
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
-    except TypeError:
+    except:
         return "Error: Please enter numeric values only."
