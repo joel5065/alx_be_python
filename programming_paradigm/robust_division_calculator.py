f73a8b73ff7406a1849bc08212fb41fe3f715995
Joel Kouakou <kouakoujoel7@gmail.com>
2025-01-16 08:05:43 +0000

    modifying the try block

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,7 +1,7 @@
 def safe_divide(numerator : float, denominator: float):
     try:
         result  = float(numerator) / float(denominator)
-        print(f"The result of the division is {result:.1f}")
+        return f"The result of the division is {result:.1f}"
     except ZeroDivisionError:
         print("Error: Cannot divide by zero.")
     except TypeError:
