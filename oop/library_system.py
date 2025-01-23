051582b4b1793b00ecd24a5f8788d0fea60ddb90
knj_general <joel5065@users.noreply.github.com>
2025-01-23 10:21:27 +0000

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -8,12 +8,12 @@ class Book:
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
-        super.__init__(title, author)
+        super().__init__(title, author)
         self.file_size = file_size
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
-        super.__init__(title, author)
+        super().__init__(title, author)
         self.page_count = page_count
 
 
