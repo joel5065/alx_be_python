c789aa4b2ea59d026b4801627bd9aac95829ed0c
knj_general <joel5065@users.noreply.github.com>
2025-01-23 10:22:33 +0000

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -18,8 +18,8 @@ class PrintBook(Book):
 
 
 class Library:
-    def __init__(self, books = []):
-        self.books = books
+    def __init__(self):
+        self.books = []
 
     def add_book(self, book):
         self.books.append(book)
