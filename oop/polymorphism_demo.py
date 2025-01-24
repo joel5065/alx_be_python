bc0c16ebff24f94a4dd9fac3b8b620753ad1697b
knj_general <joel5065@users.noreply.github.com>
2025-01-24 08:40:42 +0000

    Update polymorphism_demo.py

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -1,1 +1,11 @@
+class Shape:
+  def area(self):
+    raise NotImplementedError
 
+class Rectangle(Shape):
+  def area(length, width):
+    return length * width
+
+class Circle(Shape):
+  def area(radius):
+    return radius * math.pi
